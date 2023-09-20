02a366d7991e9e71c77123314a1ee9d36b6e07fd
kk <tangzekk@gmail.com>
2023-09-20 14:32:36 +0800

    record服务 user服务可以单独正常使用

---
+++ src/test/java/top/zekk/kkcloud/KkCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package top.zekk.kkcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
